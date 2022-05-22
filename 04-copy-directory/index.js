4cdfb6d36b5bf15107478b91715f8dc3ce48e2cd
Matveev Timofey <justshowel@gmail.com>
2022-05-23 00:51:38 +0300

    feat: Implement task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
 
+const fs = require('fs');
+const path = require('path');
+
+async function copyDir(from, to) {
+  from = path.join(__dirname, from);
+  to = path.join(__dirname, to);
+  let files = [];
+  await fs.promises.mkdir(to, {recursive: true});
+  try {
+    files = await fs.promises.readdir(from);
+  } catch (err) {
+    console.error('Could not open directory: ', err);
+    process.exit(1);
+  }
+  files = files.map(x => [x, path.join(from, x)]);
+  await Promise.all(files.map(x => fs.promises.copyFile(x[1], path.join(to, x[0]))));
+  console.log('All files have been copied');
+
+}
+
+copyDir('files', 'files-copy');
