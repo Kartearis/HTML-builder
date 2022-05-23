ccee9a1b2a8f9e412a8adddc5961188aacf844a3
Matveev Timofey <justshowel@gmail.com>
2022-05-23 14:25:18 +0300

    feat: Implement task 6

---
+++ 06-build-page/index.js
@@ -0,0 +1,86 @@
+
+const fs = require('fs');
+const path = require('path');
+
+async function copyDir(from, to, topLevel = true) {
+  let files = [];
+  if (topLevel)
+    await fs.promises.mkdir(to, {recursive: true});
+  try {
+    files = await fs.promises.readdir(from, {withFileTypes: true});
+  } catch (err) {
+    console.error('Could not open directory: ', err);
+    process.exit(1);
+  }
+  files = files.map(x => [x.name, path.join(from, x.name), x.isDirectory()]);
+  await Promise.all(files.map(x => x[2] ? copyDir(x[1], path.join(to, x[0]), false) : fs.promises.copyFile(x[1], path.join(to, x[0]))));
+  if (topLevel)
+    console.log('-> Assets have been copied.');
+
+}
+
+async function bundleCss(from, to) {
+  let files = [];
+  try {
+    files = await fs.promises.readdir(from);
+  } catch (err) {
+    console.error('Could not open directory: ', err);
+    process.exit(1);
+  }
+  let stats = [];
+  try {
+    stats = await Promise.all(files.map((file) => fs.promises.stat(path.join(from, file))));
+  } catch (err) {
+    console.error('Could not get stats for file: ', err);
+    process.exit(2);
+  }
+  const copyPromises = files.filter((file, index) => stats[index].isFile() && path.extname(file) === '.css').map(
+    file => fs.promises.readFile(path.join(from, file))
+  );
+  const targetStream = fs.createWriteStream(to);
+  targetStream.promisedWrite = function(data) {
+    return new Promise(resolve => {
+      this.write(data, resolve);
+    });
+  };
+  for (const promise of copyPromises)
+  {
+    await targetStream.promisedWrite(await promise + '\n');
+  }
+  console.log('-> Css bundling is finished.');
+}
+
+async function replaceTemplateRefs(template) {
+  const regex = /{{([\s\S]+?)}}/g;
+  const promises = [];
+  template.replace(regex, (match, p1) => {
+    try {
+      promises.push(fs.promises.readFile(path.join(__dirname, 'components', p1 + '.html')));
+      return match;
+    } catch (err) {
+      console.error(`Could not read template file (Maybe template name contains errors?) [${err}]`);
+    }
+  });
+  const replacements = await Promise.all(promises);
+  return template.replace(regex, () => replacements.shift());
+}
+
+async function buildHtml(template, destination) {
+  const templateData = await fs.promises.readFile(template);
+  const completed = await replaceTemplateRefs(templateData.toString());
+  await fs.promises.writeFile(destination, completed);
+  console.log('-> Template is finished.');
+}
+
+async function buildApp() {
+  const targetPath = path.join(__dirname, 'project-dist');
+  // execute simultaneously via all
+  await fs.promises.mkdir(targetPath, {recursive: true});
+  await copyDir(path.join(__dirname, 'assets'), path.join(targetPath, 'assets'));
+  await bundleCss(path.join(__dirname, 'styles'), path.join(targetPath, 'style.css'));
+  await buildHtml(path.join(__dirname, 'template.html'), path.join(targetPath, 'index.html'));
+  console.log('Build complete.');
+}
+
+
+buildApp();
